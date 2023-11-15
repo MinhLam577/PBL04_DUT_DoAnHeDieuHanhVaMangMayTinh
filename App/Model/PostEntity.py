8801872d5bc570683c431a59b40761b47489c214
Lâm Nhật Minh <minh32405@gmail.com>
2023-11-15 16:50:52 +0700

    Update PostEntity.py

--- App/Model/PostEntity.py
+++ App/Model/PostEntity.py
@@ -9,4 +9,3 @@ class Post(Base):
     NameUserSend = Column(String, notnull=True)
     LinkPost = Column(String, notnull=True)
     LinkImg = Column(String, notnull=True)
-Base.metadata.create_all(bind=engine)
