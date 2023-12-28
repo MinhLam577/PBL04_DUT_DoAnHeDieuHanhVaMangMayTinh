98336d9771f45130b132df5b57773ad02bb3fb27
Lâm Nhật Minh <minh32405@gmail.com>
2023-12-28 17:24:21 +0700

    ok

--- App/Model/TuongTacEntity.py
+++ App/Model/TuongTacEntity.py
@@ -3,7 +3,7 @@ from config.db import *
 class TuongTac(Base):
     __tablename__ = "TuongTac"
     IDTT = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    IDUser = Column(String, nullable=False, default=None, unique=True)
-    IDTD = Column(String, nullable=False, default=None, unique=True)
+    IDUser = Column(String(100), nullable=False, default=None, unique=True)
+    IDTD = Column(String(100), nullable=False, default=None, unique=True)
     ThoiDiem = Column(Date, nullable=False, default=None)
 Base.metadata.create_all(bind=engine)
