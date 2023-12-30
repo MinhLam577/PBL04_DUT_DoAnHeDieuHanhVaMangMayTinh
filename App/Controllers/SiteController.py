80d0e20e95f854cf212fe11eae5509e722b311e6
Lâm Nhật Minh <minh32405@gmail.com>
2023-12-30 12:06:45 +0700

    ok

--- App/Controllers/SiteController.py
+++ App/Controllers/SiteController.py
@@ -3,6 +3,7 @@ from App.Controllers.TDController import TDController
 from App.Controllers.UserController import UserController
 from App.Controllers.TuongTacController import TuongTacController
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 import json
 template = Jinja2Templates(directory="App/View/templates")
 tdController = TDController()
@@ -12,6 +13,8 @@ class SiteController:
     def index(self, request):
         listTD = tdController.GetAllTDs()
         listTD = sorted(listTD, key=lambda x: datetime.strptime(str(x['NgayTD']), '%Y-%m-%d %H:%M:%S'), reverse=True)
+        one_month_ago = datetime.now() - relativedelta(months=1)
+        listTD = [td for td in listTD if datetime.strptime(str(td['NgayTD']), '%Y-%m-%d %H:%M:%S') > one_month_ago]
         LinhVucTD = [td['LinhVucTD'] for td in listTD]
         LinhVucTD = list(set(LinhVucTD))
         ViTriTD = [td['ViTriTD'] for td in listTD]
@@ -29,6 +32,8 @@ class SiteController:
     def adminIndex(self, request, userID: str):
         listTD = tdController.GetAllTDs()
         listTD = sorted(listTD, key=lambda x: datetime.strptime(str(x['NgayTD']), '%Y-%m-%d %H:%M:%S'), reverse=True)
+        one_month_ago = datetime.now() - relativedelta(months=1)
+        listTD = [td for td in listTD if datetime.strptime(str(td['NgayTD']), '%Y-%m-%d %H:%M:%S') > one_month_ago]
         LinhVucTD = [td['LinhVucTD'] for td in listTD]
         LinhVucTD = list(set(LinhVucTD))
         ViTriTD = [td['ViTriTD'] for td in listTD]
@@ -58,6 +63,8 @@ class SiteController:
         else:
             listTD = tdController.GetAllTDs()
             listTD = sorted(listTD, key=lambda x: datetime.strptime(str(x['NgayTD']), '%Y-%m-%d %H:%M:%S'), reverse=True)
+            one_month_ago = datetime.now() - relativedelta(months=1)
+            listTD = [td for td in listTD if datetime.strptime(str(td['NgayTD']), '%Y-%m-%d %H:%M:%S') > one_month_ago]
             LinhVucTD = [td['LinhVucTD'] for td in listTD]
             LinhVucTD = list(set(LinhVucTD))
             ViTriTD = [td['ViTriTD'] for td in listTD]
@@ -89,6 +96,8 @@ class SiteController:
         if(LinhVucTD != None and DiaDiem != None and Text != None and LuongTD != None):
             listTD_Search = tdController.TimKiemTD(Text, LinhVucTD, DiaDiem, LuongTD)
             listTD_Search = sorted(listTD_Search, key=lambda x: datetime.strptime(str(x['NgayTD']), '%Y-%m-%d %H:%M:%S'), reverse=True)
+            one_month_ago = datetime.now() - relativedelta(months=1)
+            listTD_Search = [td for td in listTD_Search if datetime.strptime(str(td['NgayTD']), '%Y-%m-%d %H:%M:%S') > one_month_ago]
             LinhVucTD = None
             ViTriTD = None
             NoiTD = None
@@ -97,6 +106,7 @@ class SiteController:
             "An Giang","Bà Rịa - Vũng Tàu","Bắc Giang","Bắc Kạn","Bạc Liêu","Bắc Ninh","Bến Tre","Bình Định","Bình Dương","Bình Phước","Bình Thuận","Cà Mau","Cần Thơ","Cao Bằng","Đà Nẵng", "Đắk Lắk","Đắk Nông", "Điện Biên", "Đồng Nai", "Đồng Tháp", "Gia Lai", "Hà Giang",  "Hà Nam", "Hà Nội", "Hà Tĩnh", "Hải Dương", "Hải Phòng", "Hậu Giang", "Hòa Bình", "Hưng Yên", "Khánh Hòa", "Kiên Giang", "Kon Tum" "Lai Châu", "Lâm Đồng", "Lạng Sơn", "Lào Cai", "Long An", "Nam Định", "Nghệ An", "Ninh Bình", "Ninh Thuận", "Phú Thọ", "Phú Yên", "Quảng Bình", "Quảng Nam", "Quảng Ngãi", "Quảng Ninh", "Quảng Trị", "Sóc Trăng", "Sơn La", "Tây Ninh", "Thái Bình","Thái Nguyên","Thanh Hóa","Thừa Thiên Huế","Tiền Giang","Thành phố Hồ Chí Minh","Trà Vinh","Tuyên Quang","Vĩnh Long","Vĩnh Phúc","Yên Bái"
             ]
             listTD = tdController.GetAllTDs()
+            listTD = [td for td in listTD if datetime.strptime(str(td['NgayTD']), '%Y-%m-%d %H:%M:%S') > one_month_ago]
             LinhVucTD = [td['LinhVucTD'] for td in listTD]
             LinhVucTD = list(set(LinhVucTD))
             ViTriTD = [td['ViTriTD'] for td in listTD]
@@ -111,6 +121,8 @@ class SiteController:
             return template.TemplateResponse("formTimKiem.html", {"request": request, 'listTD': listTD_Search, 'LinhVucTD': LinhVucTD, 'DiaDiem': DiaDiem, 'ViTriTD': ViTriTD, 'NoiTD': NoiTD, 'YeuCauCongViec': YeuCauCongViec, 'user': user})
         else:
             listTD = tdController.GetAllTDs()
+            one_month_ago = datetime.now() - relativedelta(months=1)
+            listTD = [td for td in listTD if datetime.strptime(str(td['NgayTD']), '%Y-%m-%d %H:%M:%S') > one_month_ago]
             lvtd = [td['LinhVucTD'] for td in listTD]
             lvtd = list(set(lvtd))
             vttd = [td['ViTriTD'] for td in listTD]
