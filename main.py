318a8e8dd3254658e970f0dc15d041cbfb0d5dba
Lâm Nhật Minh <minh32405@gmail.com>
2024-01-01 16:56:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import traceback
 from router.indexRouter import *
 from App.auth.jwt_handler import *
 import uvicorn
-import pandas as pd
-from openpyxl import load_workbook
-from openpyxl.styles import Alignment
 if __name__ == "__main__":
     try:
         uvicorn.run("main:app", host="localhost", port=6789, reload=True)
